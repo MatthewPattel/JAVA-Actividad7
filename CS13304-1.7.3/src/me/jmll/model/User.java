0621e38a94ede26c2d70e379fd8c3f932d36cf42
Unknown <franciscobelester@gmail.com>
2019-03-17 20:09:03 -0600

    get set

--- CS13304-1.7.3/src/me/jmll/model/User.java
+++ CS13304-1.7.3/src/me/jmll/model/User.java
@@ -6,5 +6,26 @@ public class User {
 	 * getters/setters y constructor con parámetros 
 	 * username y password
 	 * */
-
+	String username = "";
+	String password = "";
+	String fullName = "";
 }
+	  // Getter
+	  public String getUsername() {
+	    return username;
+	  }
+
+	  // Setter
+	  public void setUsername(String newUsername) {
+	    this.username = newUsername;
+	  }
+	  
+	  public String getPassword(){
+		  return password;
+	  }
+	  
+	  public void setPassword (String newPassword){
+		  
+	  }
+	  
+	  
